97b409387fc40b1f46017aa3580274e4441691d2
Septian Nugraha <septian.adi.nugraha@gmail.com>
2017-12-11 14:46:56 +0700

    Commit pertama

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,63 @@
+package hello;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    @RequestMapping("/data")
+    public List<String> dataNegara(@RequestParam("pre") int s){
+    	List<String> data = new ArrayList<>();
+    	data.add("Indonesia");
+    	data.add("Malaysia");
+    	data.add("Brunei");
+    	data.add("Timor Leste");
+    	
+    	return data.stream().filter(line -> line.length() > s).collect(Collectors.toList());
+    }
+    
+    @RequestMapping("/countries")
+    public String getCountries() throws IOException{
+    	URL url = new URL("http://www.webservicex.net/country.asmx/GetCountries");
+    	URLConnection connection = url.openConnection();
+//    	For POST method
+    	connection.setDoOutput(true);
+    	connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    	connection.setRequestProperty("Content-Length", "0");
+    	
+    	InputStream stream = connection.getInputStream();
+    	InputStreamReader reader = new InputStreamReader(stream);
+    	BufferedReader buffer = new BufferedReader(reader);
+    	
+    	String line;
+    	StringBuilder builder = new StringBuilder();
+    	while((line = buffer.readLine()) != null) {
+    		builder.append(line);
+    	}
+    	return builder.toString();
+
+    }
+}
